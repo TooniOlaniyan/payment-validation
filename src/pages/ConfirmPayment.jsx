4af3c3b411929e32ce7713d2c7ca0345eea63cb7
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-21 20:56:38 +0100

    confirm payment route

---
+++ src/pages/ConfirmPayment.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import Nav from '../shared/Nav'
+
+function ConfirmPayment() {
+  return (
+    <Section>
+      <Nav />
+    </Section>
+  )
+}
+const Section = styled.div`
+  padding: 5rem 0;
+`
+
+export default ConfirmPayment
